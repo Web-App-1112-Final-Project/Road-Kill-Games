1cdbeafcec4838df02a91bbdfb26c1f708c74fab
EnyaWu <v50826v@gmail.com>
2023-04-26 15:41:48 +0800

    首頁

---
+++ Homepage/js/script.js
@@ -0,0 +1,19 @@
+// 只要往下滑，就callstickToTop
+window.onscroll = function() {
+    stickToTop()
+};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickToTop() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
