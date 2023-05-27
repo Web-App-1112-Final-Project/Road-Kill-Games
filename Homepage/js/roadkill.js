8b6c94c0c994dede159ca76234bde3d5bbdf5048
Homer Lin <guan40214@gmail.com>
2023-05-28 00:52:22 +0800

    add roadkill_game to homepage

---
+++ Homepage/js/roadkill.js
@@ -0,0 +1,33 @@
+import playGame, { initRender } from './roadkill_game/game.js';
+import { player, startAction } from './roadkill_game/player.js';
+
+const score = document.querySelector('.score')
+const startScreen = document.querySelector('.startScreen')
+const startGameForm = document.querySelector('#startGame')
+const speedInput = document.querySelector('#speed');
+const grassArea = document.querySelector('.grass')
+
+
+const start = (e) => {
+  e.preventDefault()
+  console.log('start')
+
+  let speed;
+  if (e.target[0] === speedInput) {
+    speed = parseInt(speedInput.value);
+    console.log(`The speed is ${speed}.`);
+  }
+
+  startGameForm.classList.toggle('hide')
+  startScreen.classList.toggle('hide')
+  grassArea.classList.toggle('hide')
+  initRender()
+  player.dispatch(startAction({ speed: speed }))
+  window.requestAnimationFrame(playGame)
+}
+
+startGameForm.addEventListener('submit', start)
+// document.addEventListener('keydown', pressOn)
+// document.addEventListener('keyup', pressOff)
+
+
